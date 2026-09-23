c5058480a7844bb442243afab7e2092d1ae9ff01
Prem Nirmal <prem.nirmal88@gmail.com>
2026-09-23 16:56:00 +0100

    merge conflict fix

--- pump/omnipod/common/src/main/kotlin/app/aaps/pump/omnipod/common/keys/DashBooleanPreferenceKey.kt
+++ pump/omnipod/common/src/main/kotlin/app/aaps/pump/omnipod/common/keys/DashBooleanPreferenceKey.kt
@@ -10,6 +10,7 @@ enum class DashBooleanPreferenceKey(
     private val titleResId: Int,
 ) : BooleanPreferenceKey {
 
+    SoundDeliverySuspendedNotification("AAPS.Omnipod.notification_delivery_suspended_sound_enabled", true, titleResId = R.string.omnipod_common_preferences_notification_delivery_suspended_sound_enabled),
     UseBonding("AAPS.Omnipod.Dash.use_bonding", false, titleResId = R.string.omnipod_dash_use_bonding),
     ;
 
